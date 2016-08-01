d79570c87e5b231b7e947281db5329a6923a4de1
alex <hootlex@icloud.com>
2016-08-01 05:21:16 +0300

    use website config

--- config/website.php
+++ config/website.php
@@ -1,4 +1,10 @@
 <?php
 return [
-    'logo' => '/img/logo.png'
+    'name' => 'Vue.js Feed',
+    'logo' => '/img/logo.png',
+    'icon' => '/img/favicon.ico',
+    'email' => 'team@vuejsfeed.com',
+    'facebook_url' => '#',
+    'twitter_username' => 'vuedo',
+    'twitter_url' => 'https://twitter.com/vuedo',
 ];
